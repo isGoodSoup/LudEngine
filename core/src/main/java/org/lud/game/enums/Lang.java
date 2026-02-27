45afe07605189dce9a7588c555d11651f3cb91f1
diego <sargent.pointed453@passinbox.com>
2026-02-27 13:28:35 +0100

    add: localization thu 27-02 13:28

---
+++ core/src/main/java/org/lud/game/enums/Lang.java
@@ -0,0 +1,33 @@
+package org.lud.game.enums;
+
+import org.lud.game.service.Localization;
+
+import java.util.Locale;
+
+public enum Lang {
+    EN("en"),
+    ES("es"),
+    FR("fr");
+
+    private final String langKey;
+
+    Lang(String langKey) {
+        this.langKey = langKey;
+    }
+
+    public static void nextLang() {
+        Lang[] languages = Lang.values();
+        Lang current = null;
+        for (Lang l : languages) {
+            if (l.langKey.equals(Localization.lang.getLocale().getLanguage())) {
+                current = l;
+                break;
+            }
+        }
+        int currentIndex = current != null ? current.ordinal() : 0;
+        int nextIndex = (currentIndex + 1) % languages.length;
+        Localization.lang.setLocale(Locale.forLanguageTag(languages[nextIndex].langKey));
+//        log.debug("Language changed to {}",
+//                Localization.lang.getLocale().toString().toUpperCase());
+    }
+}
