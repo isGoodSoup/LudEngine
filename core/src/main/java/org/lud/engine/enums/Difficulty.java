7b19fe808dc87f5acb8bc04bf6542a41e871e2ac
diego <sargent.pointed453@passinbox.com>
2026-03-01 19:41:14 +0100

    add: Delta AI sun 01-03 19:41

--- core/src/main/java/org/lud/engine/enums/Difficulty.java
+++ core/src/main/java/org/lud/engine/enums/Difficulty.java
@@ -1,5 +1,21 @@
 package org.lud.engine.enums;
 
+import org.lud.engine.gui.Localization;
+
 public enum Difficulty {
-    ALPHA, BETA, CORONEL, DELTA, SIGMA
+    ALPHA("difficulty.alpha"),
+    BETA("difficulty.beta"),
+    CORONEL("difficulty.coronel"),
+    DELTA("difficulty.delta"),
+    SIGMA("difficulty.sigma");
+
+    private final String labelKey;
+
+    Difficulty(String labelKey) {
+        this.labelKey = labelKey;
+    }
+
+    public String getLabelKey() {
+        return Localization.lang.t(this.labelKey);
+    }
 }
