7cbb2da34386e78ddfdd6ab0034e2f008af8d857
diego <sargent.pointed453@passinbox.com>
2026-03-04 13:28:28 +0100

    fix: made Achievement generic wed 04-03 13:18

--- core/src/main/java/org/lud/engine/service/AchievementPersistence.java
+++ core/src/main/java/org/lud/engine/service/AchievementPersistence.java
@@ -2,6 +2,7 @@ package org.lud.engine.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.lud.engine.data.Achievement;
+import org.lud.engine.interfaces.Achieveable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,7 +23,7 @@ public class AchievementPersistence{
         this.file=new File(dir,"achievements.json");
     }
 
-    public void save(Collection<Achievement> achievements){
+    public void save(Collection<Achievement<Achieveable>> achievements){
         try{
             mapper.writeValue(file,achievements);
         } catch(IOException e){
